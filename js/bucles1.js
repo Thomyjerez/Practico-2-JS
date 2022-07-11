9749f0ff1f925eb9d977d30d2a0f1f985171d828
thomas jerez <tjerez@outlook.com>
2022-07-11 19:53:39 -0300

    cambios ejercicio1

--- js/bucles1.js
+++ js/bucles1.js
@@ -1,9 +1,8 @@
 let edad = parseInt(prompt(" Ingrese su edad"))
 
-if(edad >= 1 && edad >= 17){
-document.write(" Ya puede manejar")
-}else{
-    document.write("No puede manejar")
+if (edad >= 18){
+    document.write("Ya puede conducir")
+} else {
+    document.write("Numero invalido")
 }
-
 
