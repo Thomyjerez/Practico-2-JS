f65a96456d4acba3bdaf03a93b711e1ef7c4677d
thomas jerez <tjerez@outlook.com>
2022-07-12 20:42:58 -0300

    agregue el ejercicio6

--- js/ejercicio6tp2.js
+++ js/ejercicio6tp2.js
@@ -1,2 +1,10 @@
-let i, repeticion;
+let a;
+let repeticion;
+
+for( let a = 0; a <= 30; a++){
+    for(repeticion= 0; repeticion < a; repeticion++){
+        document.write(a);
+    }
+    document.write("<br>")
+}
 
