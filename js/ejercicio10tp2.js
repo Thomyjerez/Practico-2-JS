75b251f88974ac6ae25b024632985bc1fdcae7f1
thomas jerez <tjerez@outlook.com>
2022-07-12 18:33:58 -0300

    correcciones

--- js/ejercicio10tp2.js
+++ js/ejercicio10tp2.js
@@ -8,26 +8,19 @@ const filas = parseInt(prompt("Ingrese un numero de filas"));
 const columnas = parseInt(prompt("Ingrese un numero de columnas"));
 let resultado = filas * columnas;
 
-
 document.write(`<table> <tbody>`);
 
 //este se encarga de dibujar la fila
 
-for(let indiceFila = 0; indiceFila < filas; indiceFila++){
+for (let indiceFila = 0; indiceFila < filas; indiceFila++) {
+  document.write(`<tr>`);
 
-    document.write(`<tr>`)
+  for (let indiceColumna = 0; indiceColumna < columnas; indiceColumna++) {
+    document.write(`<td> ${resultado} </td>`);
+    resultado--;
+  }
 
-    for(let indiceColumna = 0; indiceColumna < columnas; indiceColumna++ ){
-
-        document.write( `<td> ${resultado} </td>`) 
-        resultado--;
-    }
-
-      
-   document.write(`</tr>`);
-
-
+  document.write(`</tr>`);
 }
-
 
 document.write(`</tbody> </table>`);
